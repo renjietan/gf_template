f845dcd03acefb307ec08cd36609098f7962f936
tanrenjie <1694805460@qq.com>
2026-02-02 22:28:01 +0800

    1

--- test_case/main_test.go
+++ test_case/main_test.go
@@ -1,44 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-	"sync/atomic"
+	"context"
 	"testing"
-	"time"
+
+	"github.com/gogf/gf/v2/os/glog"
 )
 
-func test() {
-	fmt.Println("===================")
-}
-
 func TestContext(t *testing.T) {
-	// c := context.Background()
-	// c = context.WithValue(c, "worker", "log_processor")
-	// fmt.Println("===================", c)
-	var a atomic.Bool
-	var wg sync.WaitGroup
-	var so sync.Once
-	var b atomic.Int32
-	var sm sync.Map
-	wg.Add(10)
-	for v := range 10 {
-		go func(v int32) {
-			defer wg.Done()
-			fmt.Println("v:", v)
-			a.Store(true)
-			so.Do(test)
-			sm.Store(v, true)
-			// b = v
-			// atomic.StoreInt32(&b, 1)
-			b.Store(4)
-		}(int32(v))
-	}
-	wg.Wait()
-	time.Sleep(60 * time.Second)
-	sm.Range(func(key, value interface{}) bool {
-		fmt.Printf("Key: %v, Value: %v\n", key, value)
-		return true // 返回 true 继续遍历
-	})
-	time.Sleep(60 * time.Second)
+	ctx := context.TODO()
+	l := glog.New()
+	l.SetFlags(glog.F_TIME_TIME | glog.F_FILE_SHORT)
+	l.Print(ctx, "time and short line number")
+	l.SetFlags(glog.F_TIME_MILLI | glog.F_FILE_LONG)
+	l.Print(ctx, "time with millisecond and long line number")
+	l.SetFlags(glog.F_TIME_STD | glog.F_FILE_LONG)
+	l.Print(ctx, "standard time format and long line number")
 }
